64aba832de84ad08a805d9e192f9d94185bb2e9a
keanorific <50307478+keanorific@users.noreply.github.com>
2019-05-08 11:53:57 +1200

    Use of while loop to break out of menu

--- Spirolateral1.py
+++ Spirolateral1.py
@@ -1,14 +1,16 @@
 import turtle 
 name = "main"
 spirolateral = []
-
+list_of_spirolaterals = []
 
 def display_spiro():
+    
     print (spirolateral)
     start()
 	
 def add_spiro():
-  
+  name_of_spirolateral = int(input("What will be the name of this spirolateral?"))
+  list_of_spirolaterals.append(name_of_spirolateral)
   dig_root_num = int(input("Input a number from 2-9"))
   loop_num = 1 
   def digit_root(n): 
@@ -19,8 +21,7 @@ def add_spiro():
       test = dig_root_num * loop_num
       spirolateral.append(digit_root(test)) 
       loop_num += 1  
-
-
+  
   start()
   
 def delete_spiro(): 
@@ -33,25 +34,17 @@ def display():
   print("Input a number from 1-4") 
   
 def menu():
-  upper_lim = 5
-  lower_lim = 0
-  choice = int(input())
-  if choice > lower_lim or choice < upper_lim:
-    if choice == 1: 
-      display_spiro() 
-    if choice == 2: 
-      add_spiro() 
-    if choice == 3:
-      delete_spiro 
-    if choice == 4: 
-      exit
-  else:
-    print("That wasn't valid input")
-    start()
-	
+    choice = int(input())
+    while choice != 4:
+        if choice == 1: 
+            display_spiro() 
+        if choice == 2: 
+            add_spiro() 
+        if choice == 3:
+            delete_spiro
+            
 def start():
   display() 
   menu() 
   
 start()
-
