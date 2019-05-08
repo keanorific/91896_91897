827798deef7ec3eaa66554e787e6bb14d7dd1bdc
keanorific <50307478+keanorific@users.noreply.github.com>
2019-05-08 14:36:07 +1200

    Trying to check for elements in an array

--- Spirolateral1.py
+++ Spirolateral1.py
@@ -1,6 +1,7 @@
+#Starting lists that will be added to as the user adds more spirolaterals
 spirolateral = []
 list_of_spirolaterals = []
-
+#Displays the spirolaterals by asking the user which spirolateral they want to display then checking if that spirolateral exists in the array
 def display_spiro():
   print("List of Spirolaterals to choose from: ")
   for i in range(len(spirolateral)):
@@ -11,10 +12,10 @@ def display_spiro():
     if check_spiro == spirolateral[i][0]:
       print (spirolateral[i])
       break
-    
+#Codes for the digital root, adding the digits together for numbers having double or more digits
 def digit_root(n): 
     return (n - 1) % 9 + 1 if n else 0
-  
+#Adds
 def add_spiro():
   temp_entry_spirolateral = []
   name_of_spirolateral = str(input("What will be the name of this spirolateral? "))
@@ -32,9 +33,14 @@ def add_spiro():
 
   temp_entry_spirolateral.append(name_of_spirolateral)
   loop_num = 1
-  while loop_num < 10:
-    test = dig_root_num * loop_num
-    temp_entry_spirolateral.append(digit_root(test))
+  while True:
+    spirolateral_number = dig_root_num * loop_num
+    print(temp_entry_spirolateral)
+    if (spirolateral_number in temp_entry_spirolateral):
+      print("Element found")
+      break
+    temp_entry_spirolateral.append(digit_root(spirolateral_number))
+    print(digit_root(spirolateral_number))
     loop_num += 1
 
   spirolateral.append(temp_entry_spirolateral)
@@ -53,24 +59,24 @@ def display():
   for x in display_list: 
     print(x)
   choice = input("Input a number from 1-4: ")
-  #Prevents a error by the user inputting letters
+  #Prevents a error by the user inputting letters and instead just asks the user again
   try:
     choice = int(choice)
   except:
-    print("Please enter an integer")
+    print("Please enter an integer: ")
   
 def menu():
-      while choice != 4:
+      while choice != 4: #If the input is 4, the program will quit
           if choice == 1: 
               display_spiro() #Calls for the display_spiro to run (above)
           if choice == 2:
               add_spiro() 
           if choice == 3:
               delete_spiro()
-          display()
+          display() #If the user inputs letter or numbers ouside of 1-4, the program will ask the user again
+      print("Goodbye!")
             
-def start():
-  display() 
-  menu() 
+
+display() 
+menu() 
   
-start()
